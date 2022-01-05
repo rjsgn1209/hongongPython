599e9bd1ec54b06ab8a1895af8921dad378af63c
rjsgn1209 <huu1209@naver.com>
2022-01-05 17:51:39 +0900

    Update ch02-3, ch02-4

--- Chapter02 자료형/Ch02-2 숫자/circle.py
+++ Chapter02 자료형/Ch02-2 숫자/circle.py
@@ -1,8 +1,11 @@
-print("값을 입력하세요:")
-r = float(input(""))
-pi = 3.14
+while 1:
+    try:
+        r = float(input("값을 입력하세요: "))
+        pi = 3.14
 
-l = float(2) * pi * r
-s = r ** float(2) * pi
+        l = float(2) * pi * r
+        s = r ** float(2) * pi
 
-print(l, s)
+        print("둘레:", l, "면적:", s)
+    except:
+        continue
